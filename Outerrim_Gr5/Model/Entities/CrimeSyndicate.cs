387b8762c04869a8f416305da50c3f3ddd474a56
vik <v.weiss@htlkrems.at>
2025-02-18 12:42:10 +0100

    weiter

--- Outerrim_Gr5/Model/Entities/CrimeSyndicate.cs
+++ Outerrim_Gr5/Model/Entities/CrimeSyndicate.cs
@@ -1,6 +1,18 @@
-﻿namespace Model.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Model.Entities;
+
+[Table("CRIME_SYNDICATES")]
 public class CrimeSyndicate
 {
-    
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("SYNDICATE_ID")]
+    public int Id { get; set; }
+    [Required, StringLength(100)]
+    [Column("NAME")]
+    public string Name { get; set; }
+    [Required, StringLength(200)]
+    [Column("LOCATION")]
+    public string Location { get; set; }
 }
