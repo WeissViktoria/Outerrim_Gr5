53cf0fafe6073c7e0d1d080d6d354dd22315d98d
vik <v.weiss@htlkrems.at>
2025-02-18 12:25:29 +0100

    slide 27

--- Outerrim_Gr5/Model/Configurations/OuterrimDbContext.cs
+++ Outerrim_Gr5/Model/Configurations/OuterrimDbContext.cs
@@ -1,6 +1,36 @@
-﻿namespace Model.Configurations;
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Entities;
 
-public class OuterrimDbContext
+namespace Model.Configurations;
+
+public class OuterrimDbContext : DbContext
 {
+    public DbSet<Aircraft> Aircrafts { get; set; }
+    public DbSet<Compartment> Compartments { get; set; }
+    public DbSet<Mercenary> Mercenaries { get; set; }
+    public DbSet<Crew> Crews { get; set; }
+    
+    public OuterrimDbContext(DbContextOptions<OuterrimDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Compartment>()
+            .HasOne(c => c.Aircraft)
+            .WithMany()
+            .HasForeignKey(c => c.Aircraft_Id);
+
+        modelBuilder.Entity<Crew>()
+            .HasKey(c => new { c.Aircraft_Id, c.Mercenary_Id });
+        
+        modelBuilder.Entity<Crew>()
+            .HasOne(c => c.Aircraft)
+            .WithMany()
+            .HasForeignKey(c => c.Aircraft_Id);
+        
+        modelBuilder.Entity<Crew>()
+            .HasOne(c => c.Mercenary)
+            .WithMany()
+            .HasForeignKey(c => c.Mercenary_Id);
+    }
     
 }
