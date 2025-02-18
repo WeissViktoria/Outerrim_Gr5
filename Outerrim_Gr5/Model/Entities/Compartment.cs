d628e4328d6d48b04f54c0a3dbdb2bc88f3bf9f3
vik <v.weiss@htlkrems.at>
2025-02-18 12:14:15 +0100

    Ersten 3 fertig

--- Outerrim_Gr5/Model/Entities/Compartment.cs
+++ Outerrim_Gr5/Model/Entities/Compartment.cs
@@ -1,6 +1,15 @@
-﻿namespace Model.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Model.Entities;
+
+[Table("COMPARTMENT")]
 public class Compartment
 {
-    
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("COMPARTMENT_ID")]
+    public int Id { get; set; }
+    public Aircraft Aircraft { get; set; }
+    [Column("AIRCRAFT_ID")]
+    public int Aircraft_Id { get; set; }
 }
--- Outerrim_Gr5/Model/Entities/Crew.cs
+++ Outerrim_Gr5/Model/Entities/Crew.cs
@@ -1,6 +1,15 @@
-﻿namespace Model.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Model.Entities;
+
+[Table("AIRCRAFT_CREW_JT")]
 public class Crew
 {
+    public Aircraft Aircraft { get; set; }
+    [Column("AIRCRAFT_ID")]
+    public int Aircraft_Id { get; set; }
     
+    public Mercenary Mercenary { get; set; }
+    [Column("MERCENARY_ID")]
+    public int Mercenary_Id { get; set; }
 }
